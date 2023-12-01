1895c46d5d367be83a8b8d15d2ea29bf81ed29d4
Teun Swietkowiak <teunswietkowiak@gmail.com>
2023-12-01 16:14:25 +0100

    les 10 voltooid: boekhouding gedaan

---
+++ presentatie.py
@@ -0,0 +1,12 @@
+def presenteer(d, totaal):
+    print()
+    for k, v in d.items():
+        print(k + " : " + str(v))
+    print("==========================")
+    print("totaal : ", totaal)
+    
+
+
+
+
+
